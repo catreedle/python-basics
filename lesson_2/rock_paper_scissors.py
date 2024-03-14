ededd279ecf87c2e3df6cd566d6b40683f7875b0
Purnama S Rahayu <purnamasrahayu@gmail.com>
2024-03-14 20:56:17 +0700

    add comment

--- lesson_2/rock_paper_scissors.py
+++ lesson_2/rock_paper_scissors.py
@@ -7,7 +7,6 @@ with open('rps.json', 'r') as file:
 def clear_screen():
     print("\033[H\033[J")  # ANSI escape code to clear the screen
 
-
 def messages(message, lang="en"):
     return MESSAGE[lang][message]
 
@@ -20,16 +19,18 @@ VALID_CHOICES = {
 }
 
 VALID_CHOICES_VALUES = list(VALID_CHOICES.values())
-
 WINNING_SCORE = 3
 
 def prompt(message):
+    # display message with formatting
     print(f'>>>{message}')
 
 def get_user_input(prompt_message):
+    # return input from user while displaying formatted input prompt
     return input(f'>>>{prompt_message}').lower()
 
 def get_winner(player, computer):
+    # return the winner based on game rules
     if player == computer:
         return messages("tie")
 
@@ -46,6 +47,7 @@ def get_winner(player, computer):
     return messages("computer_win")
 
 def restart_game():
+    # ask if user want to continue playing the game
     answer = get_user_input(messages("restart"))
     while answer not in ('n', 'no', 'y', 'yes'):
         answer = get_user_input(messages("restart_invalid"))
@@ -54,6 +56,7 @@ def restart_game():
     return answer in ['y', 'yes']
 
 def validate_input(value):
+    # validate that user input is in VALID_CHOICES keys or VALID_CHOICES values
     while value not in VALID_CHOICES_VALUES:
         if value in VALID_CHOICES:
             value = VALID_CHOICES.get(value)
@@ -63,6 +66,7 @@ def validate_input(value):
     return value
 
 def update_score(winner, player_score, computer_score):
+    # update player or computer score based on the winner
     match winner:
         case 'You win!':
             player_score += 1
@@ -71,10 +75,12 @@ def update_score(winner, player_score, computer_score):
     return player_score, computer_score
 
 def display_scores(player_score, computer_score):
+    # display current score of player and computer
     prompt(messages("scores").
            format(player=player_score,computer=computer_score))
 
 def grand_winner(player_score, computer_score):
+    # return winner when one of the two opponent reaches WINNING_SCORE first
     result = ''
     if player_score == WINNING_SCORE:
         result = 'player'
@@ -94,6 +100,7 @@ def initialize_game():
     return player_score, computer_score
 
 def get_player_choice():
+    # get player choice from user input and validate the input
     choices = ', '.join(VALID_CHOICES_VALUES)
     shorthand = ', '.join(VALID_CHOICES)
 
@@ -104,15 +111,19 @@ def get_player_choice():
     return choice
 
 def get_computer_choice():
+    # randomly return a choice from VALID_CHOICES_VALUES
     computer_choice = random.choice(VALID_CHOICES_VALUES)
     return computer_choice
 
 def play_round():
+    # get player and computer choices
     choice = get_player_choice()
     computer_choice = get_computer_choice()
 
+    # display player and computer choices
     prompt(messages("display_choices").
            format(choice=choice, computer_choice=computer_choice))
+    # get winner based on those choices
     winner = get_winner(choice, computer_choice)
     return winner
 
@@ -123,6 +134,7 @@ def play_game():
     prompt(messages("welcome").format(choices=choices))
 
     while True:
+        # continue playing the game after user input to stop
         winner = play_round()
         prompt(winner)
         player_score, computer_score = update_score(winner,
